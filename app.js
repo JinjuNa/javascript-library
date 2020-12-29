24af7f87b27075448546311a97eee54036f17c2a
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-12-29 22:35:23 +0900

    db 정리

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ var app = express();
 
 var mysql = require('mysql');
 var db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'aa950307',
-    database: 'library',
-    port : '3307'
-
+    typeCast: function (field, next) {
+        if (field.type == 'VAR_STRING') {
+            return field.string();
+        }
+        return next();
+    }
 });
 
 db.connect();
@@ -25,7 +25,7 @@ app.get('/', function (req, res) {
 
 // 전체 카테고리, 각 카테고리명 보여주기
 app.get('/library/getCategory', function (req, res) {
-    db.query('SELECT * FROM category', function (error, result) {
+    db.query('SELECT * FROM library_category', function (error, result) {
         if (error) {
             console.log(error);
         }
@@ -35,7 +35,7 @@ app.get('/library/getCategory', function (req, res) {
 
 // 카테고리id 별 책 리스트의 정보 보여주기
 app.get('/library/getBookList/:cid', function (req, res) {
-    db.query(`SELECT * FROM book WHERE cid = ${req.params.cid}`, function (error, result) {
+    db.query(`SELECT * FROM library_book WHERE cid = ${req.params.cid}`, function (error, result) {
         if (error) {
             console.log(error);
         }
@@ -45,7 +45,7 @@ app.get('/library/getBookList/:cid', function (req, res) {
 
 // 책id 별 상세정보 보여주기
 app.get('/library/getBookDetail/:id', function (req, res) {
-    db.query(`SELECT a.*, b.name as categoryName FROM book a JOIN category b ON a.cid = b.id WHERE a.id = ${req.params.id}`, function (error, result) {
+    db.query(`SELECT a.*, b.name as categoryName FROM library_book a JOIN library_category b ON a.cid = b.id WHERE a.id = ${req.params.id}`, function (error, result) {
         if (error) {
             console.log(error);
         }
